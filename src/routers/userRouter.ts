cd19933242b32c1c735f15806117706383b90ee3
davicouto530 <coutodavi530@gmail.com>
2025-04-15 17:24:17 -0300

    Iniciando o projeto API Typescript de cadastro de alunos

---
+++ src/routers/userRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { getUsers, create, update, deleta } from "../controllers/userControllers";
+const route = Router();
+route.get("/alunos", getUsers);
+route.post("/create", create);
+route.put("/update/:id", update)
+route.delete("/delete/:id", deleta)
+export default route;
